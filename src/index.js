70a1dc57216c3a20ee2482b56846ed40ec64ec08
White9769 <47785867+White9769@users.noreply.github.com>
2020-10-26 00:59:18 +0300

    Fix index.js
    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+let r=Math.abs(n).toString()
+let m =r.split('');
+let arr=[];
+for(let i=m.length; i>=0;i--){
+arr.push(m[i]);
+}
+let s=arr.join('')
+return s;
 }
